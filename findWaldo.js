da76b653e9e68acd4361e13682919d18c1d09d14
Louis Riehl <louisriehl@gmail.com>
2018-09-27 13:49:38 +0000

    Refactored loop to forEach

--- findWaldo.js
+++ findWaldo.js
@@ -1,10 +1,18 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found([i]);   // execute callback
+
+  arr.forEach(function(element, index) //forEach can pass a value name AND its index if added as a parameter
+  {
+    if (element === "Waldo")
+    {
+      found(index);
     }
-  }
+  });
+  // for (var i = 0; i < arr.length; i++) {
+  //   if (arr[i] === "Waldo") {
+  //     found([i]);   // execute callback
+  //   }
+  // }
 }
 
 function actionWhenFound(index) {
