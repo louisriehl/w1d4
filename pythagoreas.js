523fc3ad67a4164fd7e7c8f23a2816db37e4c67b
Louis Riehl <louisriehl@gmail.com>
2018-09-27 14:04:56 +0000

    callback function for input.map added

--- pythagoreas.js
+++ pythagoreas.js
@@ -3,7 +3,11 @@ var input = [
   { x: 12, y: 5 },
   { x: 8, y: 15 }
 ];
-var result = input.map(/* Your code here */);
+var result = input.map(function(object)
+  {
+    var z = (object.x * object.x) + (object.y * object.y);
+    return Math.sqrt(z);
+  });
 
 console.log(result[0] === 5);
 console.log(result[1] === 13);
